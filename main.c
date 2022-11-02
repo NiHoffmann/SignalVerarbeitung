3de9250a3170b1328236f76531d30771ad0940e2
Nils Hoffmann <82971538+NiHoffmann@users.noreply.github.com>
2022-11-02 12:35:33 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -16,7 +16,9 @@
 #define PI 3.14156
 #define PI2 6.28312
 #define DeltaT 0.001
+#define EULER 0.577215664901
 
+//Signal
 typedef enum {
 	DC = 0,
 	SIN = 1,
@@ -35,9 +37,38 @@ typedef struct {
 	float phi;
 }signalGen;
 
+
+//Filter
+typedef enum {
+	OFF = 0,
+	TP = 1,
+	HP = 2,
+	BS = 3
+}filterType;
+
+typedef struct {
+	float a0;
+	float a1;
+	float a2;
+	float b1;
+	float b2;
+}filterKoef2Pol;
+
+typedef struct {
+	filterType type;
+	float freq;
+	float bwidth;
+	filterKoef2Pol koef;
+	float x1;
+	float x2;
+	float y1;
+	float y2;
+}filter;
+
 //*******************************************************************
 signalGen sigA = {{0,0,0,0},0};
 signalGen sigB = {{0,0,0,0},0};
+filter fil = {0,0.0,0.0,{0.0,0.0,0.0,0.0,0.0},0.0,0.0,0.0,0.0};
 
 void idToSig(signalGen* sigGen, char *str){
 	signal sig;
@@ -47,13 +78,51 @@ void idToSig(signalGen* sigGen, char *str){
 	}
 	sig.deltaPhi = PI2 * sig.freq * DeltaT;
 
-	TIM2->DIER = TIM_DIER_UDE;
+	lockTimer();
 	sigGen->sig = sig;
-	TIM2->DIER =TIM_DIER_UIE;
-
-
+	unlockTimer();
 
 	//signal->counter = 0;
+}
+
+float calculateFilterValue(filter f,float x0){
+	float y = f.koef.a0 * x0 + f.koef.a1 * f.x1 + f.koef.a2 * f.x2 + f.koef.b1 * f.y1 + f.koef.b2 * f.y2;
+	f.x2 = f.x1;
+	f.x1 = x0;
+	f.y2 = f.y1;
+	f.y1 = y;
+	return y;
+}
+
+void idToFil(filter* f, char *str){
+	filterKoef2Pol fk2 = {0.0,0.0,0.0,0.0,0.0};
+	if(sscanf(str,"%d;%f;%f",&(f->type),&(f->freq),&(f->bwidth)) != 3){
+		uartPrintf("Error\r\n");
+		return;
+	}
+	float f0 = 0.0;
+	float r = 0.0;
+	float k = 0.0;
+	switch(f->type){
+		case OFF: break;
+		case TP:
+			f0 = sqrt(PI2*f->freq*DeltaT);
+			r = pow(EULER,-1*f0);
+			k = (1-(2*r) * cos(f0) + pow(r,2)) / 4;
+			fk2.a0 = k;
+			fk2.a1 = 2*k;
+			fk2.a2 = k;
+			fk2.b1 = 2*r*cos(f0);
+			fk2.b2 = -1*pow(r,2);
+
+			lockTimer();
+			f->koef = fk2;
+			unlockTimer();
+
+			return;
+		case HP: break;
+		case BS: break;
+	}
 }
 
 
@@ -61,6 +130,7 @@ void fillStruct(char *str){
 	switch(str[0]){
 		case 'A': idToSig(&sigA, str+2 ); break;
 		case 'B': idToSig(&sigB, str+2 ); break;
+		case 'F': idToFil(&fil, str+2); break;
 	}
 }
 
@@ -78,10 +148,13 @@ float sinSig(signalGen* sig){
 }
 
 float sawSig(signalGen* sig){
-	return 0.0;
+	return (sig->sig.amp*(sig->phi/PI2));
 }
 
 float recSig(signalGen* sig){
+	if(sig->phi <= PI){
+		return sig->sig.amp;
+	}
 	return 0.0;
 }
 
@@ -92,22 +165,28 @@ float calculateValue(signalGen* sig){
 	switch(sig->sig.type){
 		case DC: return dcSig(sig);
 		case SIN: return sinSig(sig);
-		case SAW: break;
-		case REC: break;
+		case SAW: return sawSig(sig);
+		case REC: return recSig(sig);
 	}
 	return 0.0;
 }
 
 void timerInterrupt(){
 	static const float gain = (float)0xFFF/3000;
-	int val = gain*(1500+calculateValue(&sigA)+calculateValue(&sigB));
-	if(val > 0xFFF){
-		val = 0xFFF;
-	}else if(val < 0){
-		val = 0;
+	int valDac1 = gain*(1500+calculateValue(&sigA)+calculateValue(&sigB));
+	if(valDac1 > 0xFFF){
+		valDac1 = 0xFFF;
+	}else if(valDac1 < 0){
+		valDac1 = 0;
 	}
+	dacSet(1, valDac1);
 
-	dacSet(1, val);
+	//unsigned valAdc1 = adcGet(1);
+
+
+	//unsigned valDac2 = valAdc1;
+	//uartPrintf("%d",valAdc1);
+	//dacSet(2, valDac2);
 }
 
 int main(void)
@@ -115,6 +194,8 @@ int main(void)
   uartInit();
   lcdInit();
   dacInit(1);
+  dacInit(2);
+  adcInit(1);
   timerInit(1e6 * DeltaT, timerInterrupt);
   
   lcdPrintf( 0, 0, 20, __DATE__ "," __TIME__ );
